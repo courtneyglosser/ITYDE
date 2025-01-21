9aa8f9c07bc88e1c88a935d049d15b746fa0c5de
CourtneyG <courtney.glosser@appliedfundamentals.net>
2025-01-21 14:40:09 -0500

    Adding basic platforms with collider placeholders.

--- game_script.js
+++ game_script.js
@@ -1,5 +1,6 @@
 
 
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -19,6 +20,7 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var platforms;
 
 function preload ()
 {
@@ -32,7 +34,29 @@ function create ()
     this.add.tileSprite(0,0, this.scale.width, this.scale.height,"sky")
         .setOrigin(0, 0)
         .setScrollFactor(0);
-    console.log("Create function");
+
+
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = this.physics.add.staticGroup();
+
+    //  Here we create the ground.
+    //  Scale it to fit the width of the game (the original sprite is 32x32 in size)
+    let scale = this.scale.width / 32;
+    let ground = platforms.create(0, 568, 'ground')
+        .setOrigin(0, 0)
+        .setScale(scale, 2)
+        .refreshBody();
+
+    //  Now let's create some ledges
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    //  Collide the player and the stars with the platforms
+//    this.physics.add.collider(player, platforms);
+//    this.physics.add.collider(stars, platforms);
+//    this.physics.add.collider(bombs, platforms);
+
 }
 
 function update ()
